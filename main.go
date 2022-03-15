1b67c6d80cc762be0e02b23485f1fd1c41f3622a
Devirex <naibaf_11@yahoo.de>
2022-03-15 10:55:05 +0100

    Change TCPDump

--- main.go
+++ main.go
@@ -174,7 +174,8 @@ func runCapture() error {
 
 		log.Infof("Creating TCPDump")
 		log.Infof("%v , %v", ArkimeOptions.Interface ,ArkimeOptions.Bpf)
-		Cmd := exec.Command("/usr/local/bin/tcpdump", "-i", ArkimeOptions.Interface, "\""+ ArkimeOptions.Bpf + "\"")
+		tcpdump := ("/usr/local/bin/tcpdump -i " + ArkimeOptions.Interface + " \""+ ArkimeOptions.Bpf + "\" -S -U -w - > /tmp/rpcapd"
+		Cmd := exec.Command("/bin/bash", "-c" , tcpdump)
 		var out bytes.Buffer
 		var stderr bytes.Buffer
 		Cmd.Stdout = &out
